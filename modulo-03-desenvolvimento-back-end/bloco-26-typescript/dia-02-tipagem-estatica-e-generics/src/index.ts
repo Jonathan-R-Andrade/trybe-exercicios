025d9d5c2239017d01f2542e8c606d48e5bb233d
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-04 18:36:50 -0300

    exercicios/26.2 | recria a função filter

--- modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/src/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/src/index.ts
@@ -4,6 +4,7 @@ import CommonPizza from "./interfaces/CommonPizza";
 import SweetPizza from "./interfaces/SweetPizza";
 import VegetarianPizza from "./interfaces/VegetarianPizza";
 import pickUpPassenger from "./scripts/pickUpPassenger";
+import myFilter from "./utils/myFilter";
 
 const car = new Car('volkswagen', Color.PRATA, 4);
 
@@ -29,3 +30,16 @@ const sweetPizzas: SweetPizza[] = [
 console.log(commonPizzas);
 console.log(vegetarianPizzas);
 console.log(sweetPizzas);
+
+console.log('\n\n=================================================================\n\n');
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log('Array without filter: ');
+console.log(numbers, '\n');
+
+console.log('Array with myFilter: ');
+console.log(myFilter(numbers, (num) => num % 2 === 0), '\n');
+
+console.log('Array with filter: ');
+console.log(numbers.filter((num) => num % 2 === 0), '\n');
