ad6884f13a794786f8cf7e2c980675ed01956dad
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-04 13:18:42 -0300

    exercicios/26.2 | cria as pizzas

--- modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/src/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/src/index.ts
@@ -1,7 +1,18 @@
 import Car from "./classes/Car";
 import Color from "./enums/Color";
+import Pizza from "./interfaces/Pizza";
 import pickUpPassenger from "./scripts/pickUpPassenger";
 
 const car = new Car('volkswagen', Color.PRATA, 4);
 
 pickUpPassenger(car);
+
+console.log('\n\n=================================================================\n\n');
+
+const pizzaCalabresa: Pizza = { flavor: 'Calabresa', slices: 4 }
+const pizzaMarguerita: Pizza = { flavor: 'Marguerita', slices: 6 }
+const pizzaNutela: Pizza = { flavor: ' Nutela', slices: 8 }
+
+console.log(pizzaCalabresa);
+console.log(pizzaMarguerita);
+console.log(pizzaNutela);
