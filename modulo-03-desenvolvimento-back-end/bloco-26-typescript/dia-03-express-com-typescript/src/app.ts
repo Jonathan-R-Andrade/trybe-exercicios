24f6fc745a4190673da46ef0893ddc6d098a2410
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-05 01:41:46 -0300

    exercicios/26.3 | adiciona rota para criar usuário

--- modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/src/app.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/src/app.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import usersRouter from './routers/usersRouter';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/users', usersRouter);
 
 export default app;
---
+++ modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/src/routers/usersRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import UserController from "../controllers/userController";
+
+const usersRouter = Router();
+
+const userController = new UserController();
+
+usersRouter.post('/', userController.create);
+
+export default usersRouter;
