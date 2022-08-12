bdacc9b5aaad0f30f5365106fa24dc92e3f74c70
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-11 23:10:05 -0300

    exercicios/26.3 | adiciona rotas

--- modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/blogPosts-api/src/app.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/blogPosts-api/src/app.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import postsRouter from './routers/postsRouter';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/posts', postsRouter);
 
 export default app;
