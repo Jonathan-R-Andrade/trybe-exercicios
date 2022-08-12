a3e33a6c36c2e3ec394b94cc62766b8eda5b7e58
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-11 23:23:53 -0300

    exercicios/26.3 | adiciona middleware de erro

--- modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/blogPosts-api/src/app.ts
+++ modulo-03-desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/blogPosts-api/src/app.ts
@@ -1,5 +1,7 @@
+import 'express-async-errors';
 import dotenv from 'dotenv';
 import express from 'express';
+import handleErrors from './middlewares/handleErrors';
 import postsRouter from './routers/postsRouter';
 
 dotenv.config();
@@ -9,5 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/posts', postsRouter);
+
+app.use(handleErrors);
 
 export default app;
