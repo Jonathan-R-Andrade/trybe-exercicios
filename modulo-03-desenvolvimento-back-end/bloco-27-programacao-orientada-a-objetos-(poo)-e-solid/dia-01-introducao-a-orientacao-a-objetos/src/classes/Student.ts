449a218eb451586d3285d5e3658ebf7590aa6fd6
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-13 01:18:02 -0300

    exercicios/27.1 | adiciona classe Student

---
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-01-introducao-a-orientacao-a-objetos/src/classes/Student.ts
@@ -0,0 +1,8 @@
+export default class Student {
+  constructor(
+    public name: string,
+    public enrollment: string,
+    public testsGrades: number[],
+    public worksGrades: number[],
+  ) { }
+}
