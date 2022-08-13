921d294996c7174374836cd1168384b3916f032b
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-13 03:17:06 -0300

    exercicios/27.1 | adiciona métodos
    adiciona métodos na classe Date para verificar a data

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-01-introducao-a-orientacao-a-objetos/src/classes/Date.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-01-introducao-a-orientacao-a-objetos/src/classes/Date.ts
@@ -3,5 +3,29 @@ export default class Date {
     public day: number,
     public month: number,
     public year: number,
-  ) { }
+  ) {
+    if (!this._isAValidDate()) {
+      this.day = 1;
+      this.month = 1;
+      this.year = 1900;
+    }
+  }
+
+  private _isAValidDate(): boolean {
+    if (this.day < 1 || this.day > 31) return false;
+    if (this.month < 1 || this.month > 12) return false;
+    if (this.year < 1900) return false;
+    if (this.day > this.getLastDayOfMonth()) return false;
+    return true;
+  }
+
+  public getLastDayOfMonth(): number {
+    if (this.month === 2) return this.isLeapYear() ? 29 : 28;
+    if (this.month <= 7) return this.month % 2 === 0 ? 30 : 31;
+    return this.month % 2 === 0 ? 31 : 30;
+  }
+
+  public isLeapYear(): boolean {
+    return (this.year % 400 === 0) || (this.year % 100 !== 0 && this.year % 4 === 0);
+  }
 }
