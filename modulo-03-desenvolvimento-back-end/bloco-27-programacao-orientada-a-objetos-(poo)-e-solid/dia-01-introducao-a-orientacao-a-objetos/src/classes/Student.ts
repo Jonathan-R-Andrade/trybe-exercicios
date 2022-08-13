a0079831c0ea1e7dcaa0fb3532b71506dce1e64a
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-13 01:36:56 -0300

    exercicios/27.1 | adiciona métodos
    adiciona métodos na classe Student

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-01-introducao-a-orientacao-a-objetos/src/classes/Student.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-01-introducao-a-orientacao-a-objetos/src/classes/Student.ts
@@ -5,4 +5,14 @@ export default class Student {
     public testsGrades: number[],
     public worksGrades: number[],
   ) { }
+
+  public sumGrades(): number {
+    const exams = this.testsGrades.reduce((acc, grade) => acc + grade);
+    const works = this.worksGrades.reduce((acc, grade) => acc + grade);
+    return exams + works;
+  }
+
+  public averageGrade(): number {
+    return this.sumGrades() / (this.testsGrades.length + this.worksGrades.length);
+  }
 }
