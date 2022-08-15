37f88e19ed9713c04373a51a2e62fca0e0ff2320
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-15 03:07:02 -0300

    exercicios/27.2 | testa a classe Teacher

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-02-heranca-e-interfaces/src/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-02-heranca-e-interfaces/src/index.ts
@@ -2,6 +2,7 @@ import Date from "./classes/Date";
 import Person from "./classes/Person";
 import Student from "./classes/Student";
 import Subject from "./classes/Subject";
+import Teacher from "./classes/Teacher";
 
 const person1 = new Person('João', new Date(1, 1, 1996));
 const person2 = new Person('Maria', new Date(2, 2, 1998));
@@ -46,3 +47,22 @@ const subjects = [
 ];
 
 console.log('Disciplinas: ', subjects.map((subject) => subject.name), '\n');
+
+console.log('----------------------------------------------------');
+
+const teachers = [
+  new Teacher('Teacher-1', new Date(1, 2, 1980), 4000, subjects[0]),
+  new Teacher('Teacher-2', new Date(3, 4, 1981), 4000, subjects[1]),
+  new Teacher('Teacher-3', new Date(5, 6, 1982), 4000, subjects[2]),
+];
+
+teachers.forEach((teacher) => {
+  console.log(`
+Nome: ${teacher.name}
+Data de nascimento: ${teacher.birthDate}
+Registro: ${teacher.registration}
+Disciplina: ${teacher.subject}
+Data de admissão: ${teacher.admissionDate.format('dd/mm/aaaa')}
+Salário: ${teacher.salary}
+  `);
+});
