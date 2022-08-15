4c9bbeefd552478a1917194a23c64685ff4eb5ae
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-15 00:34:03 -0300

    exercicios/27.2 | testa a classe Student

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-02-heranca-e-interfaces/src/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-02-heranca-e-interfaces/src/index.ts
@@ -1,8 +1,37 @@
 import Date from "./classes/Date";
 import Person from "./classes/Person";
+import Student from "./classes/Student";
 
 const person1 = new Person('João', new Date(1, 1, 1996));
 const person2 = new Person('Maria', new Date(2, 2, 1998));
 
+console.log();
 console.log(`${person1.name} nasceu no dia ${person1.birthDate}`);
 console.log(`${person2.name} nasceu no dia ${person2.birthDate}`);
+
+console.log('\n----------------------------------------------------');
+
+const students = [
+  new Student('Student-1', new Date(1, 2, 1996)),
+  new Student('Student-2', new Date(3, 4, 1997)),
+  new Student('Student-3', new Date(5, 6, 1998)),
+  new Student('Student-4', new Date(7, 8, 1999)),
+  new Student('Student-5', new Date(9, 10, 2000)),
+]
+
+const randomGrade = () => Math.random() * 10;
+
+students.forEach((student) => {
+  student.examsGrades = [randomGrade(), randomGrade(), randomGrade(), randomGrade()];
+  student.worksGrades = [randomGrade(), randomGrade()];
+});
+
+students.forEach((student) => {
+  console.log(`
+Nome: ${student.name}
+Data de nascimento: ${student.birthDate}
+Matrícula: ${student.enrollment}
+Soma das notas: ${student.sumGrades()}
+Média das notas: ${student.averageGrade()}
+  `);
+});
