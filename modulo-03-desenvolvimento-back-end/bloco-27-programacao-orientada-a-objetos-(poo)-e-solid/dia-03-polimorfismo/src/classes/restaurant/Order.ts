962a0912dc50b475ff096c815d402c2dc8c4b8fc
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-16 01:17:48 -0300

    exercicios/27.3 | altera a classe Order
    troca Customer para Person e adiciona validações

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/classes/restaurant/Order.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/classes/restaurant/Order.ts
@@ -1,22 +1,32 @@
-import Customer from "./Customer";
+import Person from "../Person";
 import OrderItem from "./OrderItem";
 
-export default class Order {
-  constructor(
-    public customer: Customer,
-    public orderItems: OrderItem[],
-    public paymentMethod: 'cash' | 'card',
-    public discount?: number,
-  ) { }
+export type PaymentMethod = 'dinheiro' | 'cartão' | 'vale';
 
-  public totalToPay(): number {
-    return this.orderItems.reduce((acc, { price }) => acc + price, 0);
+export default class Order {
+  private _items!: OrderItem[];
+  private _discount?: number;
+
+  constructor(
+    public client: Person,
+    public paymentMethod: PaymentMethod,
+    items: OrderItem[],
+    discount?: number,
+  ) {
+    if (items.length < 1) throw new Error('The order must have at least one item.');
+    if (discount && discount < 0) throw new Error('The discount cannot be negative.');
+    this._items = items;
+    this._discount = discount;
   }
 
-  public totalToPayWithDiscount(): number {
-    const totalToPay = this.totalToPay();
-    const discount = this.discount && this.discount > 0
-      ? this.discount * totalToPay : 0;
-    return totalToPay - discount;
+  public calculateTotal(): number {
+    return this._items.reduce((acc, { price }) => acc + price, 0);
+  }
+
+  public calculateTotalWithDiscount(): number {
+    const calculateTotal = this.calculateTotal();
+    const discount = this._discount && this._discount > 0
+      ? this._discount * calculateTotal : 0;
+    return calculateTotal - discount;
   }
 }
