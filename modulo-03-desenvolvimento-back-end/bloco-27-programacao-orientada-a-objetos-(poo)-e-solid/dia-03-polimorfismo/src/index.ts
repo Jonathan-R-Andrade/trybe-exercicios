9418eced64fac79dada9b4861ccc88d31a8daff6
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-16 00:46:20 -0300

    exercicios/27.3 | altera o arquivo index.ts
    altera o arquivo para usar as novas classes

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/index.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/index.ts
@@ -1,19 +1,12 @@
 import Date from "./classes/Date";
+import EvaluationResult from "./classes/EvaluationResult";
+import Exam from "./classes/Exam";
 import Student from "./classes/Student";
 import Subject from "./classes/Subject";
 import Teacher from "./classes/Teacher";
+import Work from "./classes/Work";
 
-const students = [
-  new Student('Student-1', new Date(1, 2, 2000)),
-  new Student('Student-2', new Date(3, 4, 2001)),
-];
-
-const randomGrade = () => Math.random() * 10;
-
-students.forEach((student) => {
-  student.examsGrades = [randomGrade(), randomGrade(), randomGrade(), randomGrade()];
-  student.worksGrades = [randomGrade(), randomGrade()];
-});
+const randomGrade = (max: number) => Math.round(Math.random() * max);
 
 const subjects = [
   new Subject('Matemática'),
@@ -24,6 +17,19 @@ const teachers = [
   new Teacher('Teacher-1', new Date(1, 2, 1980), 4000, subjects[0]),
   new Teacher('Teacher-2', new Date(3, 4, 1981), 4000, subjects[1]),
 ];
+
+const works = [new Work(50, teachers[0]), new Work(50, teachers[1])];
+const exams = [new Exam(25, teachers[0]), new Work(25, teachers[1])];
+
+const students = [
+  new Student('Student-1', new Date(1, 2, 2000)),
+  new Student('Student-2', new Date(3, 4, 2001)),
+];
+
+students[0].addEvaluationResult(new EvaluationResult(works[0], randomGrade(50)));
+students[0].addEvaluationResult(new EvaluationResult(exams[0], randomGrade(25)));
+students[1].addEvaluationResult(new EvaluationResult(works[1], randomGrade(50)));
+students[1].addEvaluationResult(new EvaluationResult(exams[1], randomGrade(25)));
 
 students.forEach((student) => {
   console.log(`
