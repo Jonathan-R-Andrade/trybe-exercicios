19e32a69482f0dcbd75f083349e612bd595696bf
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-16 00:10:27 -0300

    exercicios/27.3 | altera a classe Evaluation
    torna a classe abstrata e remove a atributo type

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/classes/Evaluation.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/classes/Evaluation.ts
@@ -1,26 +1,17 @@
 import Teacher from "./Teacher";
 
-export type EvaluationType = 'prova' | 'trabalho';
-
-export default class Evaluation {
+export default abstract class Evaluation {
   private _score: number = 0;
 
   constructor(
     score: number,
-    public teacher: Teacher,
-    public type: EvaluationType
+    public teacher: Teacher
   ) {
     this.score = score;
   }
 
   private _checkScore(score: number): void {
     if (score < 0) throw new Error('The score cannot be negative.');
-    else if (this.type === 'prova' && score > 25) {
-      throw new Error('The score cannot exceed 25 points.');
-    }
-    else if (this.type === 'trabalho' && score > 50) {
-      throw new Error('The score cannot exceed 50 points.');
-    }
   }
 
   public get score(): number {
