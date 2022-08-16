6bfcc669631ebbeb248b09621d5d320345be4f55
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-16 01:01:42 -0300

    exercicios/27.3 | altera a classe OrderItem
    adiciona getters e setters

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/classes/restaurant/OrderItem.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-03-polimorfismo/src/classes/restaurant/OrderItem.ts
@@ -1,6 +1,31 @@
 export default class OrderItem {
-  constructor(
-    public name: string,
-    public price: number,
-  ) { }
+  private _name!: string;
+  private _price!: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(name: string) {
+    if (name.length < 3) {
+      throw new Error('The name must be at least 16 characters long.');
+    }
+    this._name = name;
+  }
+
+  public get price(): number {
+    return this._price;
+  }
+
+  public set price(price: number) {
+    if (price < 0) {
+      throw new Error('The price cannot be negative.');
+    }
+    this._price = price;
+  }
 }
