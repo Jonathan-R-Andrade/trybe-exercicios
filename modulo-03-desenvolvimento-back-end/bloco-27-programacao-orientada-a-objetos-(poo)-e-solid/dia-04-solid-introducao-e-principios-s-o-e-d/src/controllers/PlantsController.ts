5bc665072040a8cae398f33b91b02031197c2cc9
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-08-28 23:12:53 -0300

    exercicios/27.4 | altera PlantsController

--- modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-04-solid-introducao-e-principios-s-o-e-d/src/controllers/PlantsController.ts
+++ modulo-03-desenvolvimento-back-end/bloco-27-programacao-orientada-a-objetos-(poo)-e-solid/dia-04-solid-introducao-e-principios-s-o-e-d/src/controllers/PlantsController.ts
@@ -3,43 +3,41 @@ import { StatusCodes } from 'http-status-codes';
 import PlantsService from '../services/PlantsService';
 
 class PlantsController {
-  private plantsService;
-
-  constructor(plantsService: PlantsService = new PlantsService()) {
-    this.plantsService = plantsService;
-  }
-
   public getPlants = async (_req: Request, res: Response): Promise<void> => {
-    const plants = await this.plantsService.getPlants();
+    const plants = await PlantsService.getPlants();
     res.status(StatusCodes.OK).json(plants);
   }
 
   public getPlantById = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
-    const plant = await this.plantsService.getPlantById(id);
-    res.status(StatusCodes.OK).json(plant);
+    const plant = await PlantsService.getPlantById(id);
+    const status = plant ? StatusCodes.OK : StatusCodes.NOT_FOUND;
+    res.status(status).json(plant);
   }
 
   public removePlantById = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
-    await this.plantsService.removePlantById(id);
-    res.status(StatusCodes.NO_CONTENT).end();
+    const removed = await PlantsService.removePlantById(id);
+    const status = removed ? StatusCodes.NO_CONTENT : StatusCodes.NOT_FOUND;
+    res.status(status).end();
   }
 
   public getPlantsThatNeedsSunWithId = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
-    const plants = await this.plantsService.getPlantsThatNeedsSunWithId(id);
-    res.status(StatusCodes.OK).json(plants);
+    const plant = await PlantsService.getPlantsThatNeedsSunWithId(id);
+    const status = plant ? StatusCodes.OK : StatusCodes.NOT_FOUND;
+    res.status(status).json(plant);
   }
 
   public editPlant = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
-    const plant = await this.plantsService.editPlant(id, req.body);
-    res.status(StatusCodes.OK).json(plant);
+    const plant = await PlantsService.editPlant(id, req.body);
+    const status = plant ? StatusCodes.OK : StatusCodes.NOT_FOUND;
+    res.status(status).json(plant);
   }
 
   public savePlant = async (req: Request, res: Response): Promise<void> => {
-    const plant = await this.plantsService.savePlant(req.body);
+    const plant = await PlantsService.savePlant(req.body);
     res.status(StatusCodes.CREATED).json(plant);
   }
 }
