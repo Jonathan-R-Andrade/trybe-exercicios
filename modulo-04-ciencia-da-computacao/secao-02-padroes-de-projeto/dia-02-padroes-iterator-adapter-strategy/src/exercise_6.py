253326601e2761443d022eec9d6d57a379497fa7
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-10-28 01:48:03 -0300

    exercicios/34.2 | finaliza exercício 6

--- modulo-04-ciencia-da-computacao/secao-02-padroes-de-projeto/dia-02-padroes-iterator-adapter-strategy/src/exercise_6.py
+++ modulo-04-ciencia-da-computacao/secao-02-padroes-de-projeto/dia-02-padroes-iterator-adapter-strategy/src/exercise_6.py
@@ -1,20 +1,18 @@
+import impostos
+
+
 class Orcamento:
-    def __init__(self, valor):
+    def __init__(self, valor: float | int):
         self.valor = valor
 
-    def calcular_imposto(self, imposto):
-        if imposto == "ISS":
-            return self.__calcular_iss()
-        elif imposto == "ICMS":
-            return self.__calcular_icms()
-
-    def __calcular_iss(self):
-        return self.valor * 0.1
-
-    def __calcular_icms(self):
-        return self.valor * 0.06
+    def calcular_imposto(self, imposto: impostos.Imposto):
+        return imposto.calcular(self.valor)
 
 
-orcamento = Orcamento(1000)
-print(f"ISS: {orcamento.calcular_imposto('ISS')}")
-print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
+if __name__ == "__main__":
+    orcamento = Orcamento(1000)
+
+    print(f"ISS: {orcamento.calcular_imposto(impostos.ISS)}")
+    print(f"ICMS: {orcamento.calcular_imposto(impostos.ICMS)}")
+    print(f"PIS: {orcamento.calcular_imposto(impostos.PIS)}")
+    print(f"COFINS: {orcamento.calcular_imposto(impostos.COFINS)}")
