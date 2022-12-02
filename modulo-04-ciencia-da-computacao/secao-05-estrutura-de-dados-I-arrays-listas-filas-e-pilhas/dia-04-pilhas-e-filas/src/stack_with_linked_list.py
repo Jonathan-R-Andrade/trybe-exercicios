be32f329d338383f9cb9d7cccf884c8934928f6e
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2022-12-02 18:27:20 -0300

    exercicios/37.4 | finaliza exercício 2

---
+++ modulo-04-ciencia-da-computacao/secao-05-estrutura-de-dados-I-arrays-listas-filas-e-pilhas/dia-04-pilhas-e-filas/src/stack_with_linked_list.py
@@ -0,0 +1,25 @@
+from linked_list import LinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.linked_list = LinkedList()
+
+    def __len__(self):
+        return len(self.linked_list)
+
+    # Complexity O(1)
+    def push(self, value):
+        self.linked_list.insert_last(value)
+
+    # Complexity O(1)
+    def pop(self):
+        return self.linked_list.remove_last()
+
+    # Complexity O(1)
+    def peek(self):
+        return self.linked_list.get_element_at(len(self) - 1)
+
+    # Complexity O(1)
+    def is_empty(self):
+        return self.linked_list.is_empty()
